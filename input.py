cbc1bc36b99e83b3dd46acd5da2042d34a16979d
Jonathan Hanley <jonathanhanley1234@gmail.com>
2019-02-28 18:22:10 +0000

    Default Changelist

--- input.py
+++ input.py
@@ -1,20 +1,21 @@
 import shelve
 from DONT_FUCKING_DELETE_THIS import Photo
-
-shelve_file =shelve.open('shelves/a_test')
-fin=open('input_files/a_example.txt')
-id=0
-c=False
-for line in fin.readlines():
-    if not c:
-        c=True
-        continue
-    else:
-        l=line.split()
-        o=l[0]
-        num=l[1]
-        tags=l[2:]
-        photo=Photo(o,num,tags)
-        shelve_file[str(id)]=photo
-        id+=1
-shelve_file.close()
+files=['a','b','c','d','e']
+for file in files:
+    shelve_file =shelve.open('shelves/%s'%(file))
+    fin=open('input_files/%s.txt'%(file))
+    id=0
+    c=False
+    for line in fin.readlines():
+        if not c:
+            c=True
+            continue
+        else:
+            l=line.split()
+            o=l[0]
+            num=l[1]
+            tags=l[2:]
+            photo=Photo(o,num,tags)
+            shelve_file[str(id)]=photo
+            id+=1
+    shelve_file.close()
