dcb71a00f2cd388914053d659de3c2439ef8095f
Jonathan Hanley <jonathanhanley1234@gmail.com>
2019-02-28 19:27:42 +0000

    Default Changelist

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from reading_object import readPhotos
+'''
+                               88 88                      88 88
+                               88 88                      88 88
+                               88 88                      88 88
+8b,dPPYba,  ,adPPYba,  ,adPPYb,88 88,dPPYba,  88       88 88 88
+88P'   "Y8 a8P_____88 a8"    `Y88 88P'    "8a 88       88 88 88
+88         8PP""""""" 8b       88 88       d8 88       88 88 88
+88         "8b,   ,aa "8a,   ,d88 88b,   ,a8" "8a,   ,a88 88 88
+88          `"Ybbd8"'  `"8bbdP"Y8 8Y"Ybbd8"'   `"YbbdP'Y8 88 88
+'''
