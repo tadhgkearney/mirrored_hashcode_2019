d6ac2b325947e800fea4d9e34e804367a220fced
Owner <117317853@umail.ucc.ie>
2019-02-28 18:22:06 +0000

    Default Changelist

---
+++ slideshow.py
@@ -0,0 +1,9 @@
+class SlideShow:
+    def __init__(self):
+        self._slideShow = []
+
+    def add_photo(self, photo):
+        self._slideShow.append(photo)
+
+    def output(self):
+        # TODO This stuff
